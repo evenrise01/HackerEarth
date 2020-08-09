4d1cad32808249187e4f0c777c084efea10dd7b8
evenrise01 <63799338+evenrise01@users.noreply.github.com>
2020-08-09 18:01:07 +0530

    Create prime-minister's-number.cpp

---
+++ prime-minister's-number.cpp
@@ -0,0 +1,42 @@
+//https://www.hackerearth.com/practice/math/number-theory/primality-tests/practice-problems/algorithm/prime-ministers-number/description/
+
+#include<bits/stdc++.h>
+#define pb push_back
+#define endl "\n"
+using namespace std;
+
+bool isPrime(long n) {
+
+	if (n == 1) {
+		return false;
+
+	}
+
+	long i = 2;
+	while (i * i <= n) {
+		if (n % i == 0) {
+			return false;
+		}
+		i += 1;
+	}
+	return true;
+}
+
+long digitSum(long no) {
+	return no == 0 ? 0 : no % 10 + digitSum(no / 10);
+}
+
+bool check(long n) {
+	return (isPrime(n) && isPrime(digitSum(n)));
+}
+
+int main() {
+	long a, b;
+	cin >> a >> b;
+	for (long i = a; i <= b; i++) {
+		if (check(i)) {
+			cout << i << " ";
+		}
+	}
+	return 0;
+}
